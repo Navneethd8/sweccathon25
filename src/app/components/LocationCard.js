e31d85387f9d354a61db9c7948ac8ca4a2092bca
Navneeth Dhamotharan <navneethdhamotharan@gmail.com>
2025-09-18 12:28:14 +0400

    more location card changes

--- src/app/components/LocationCard.js
+++ src/app/components/LocationCard.js
@@ -1,23 +1,20 @@
 'use client';
 
-import { useRouter } from 'next/navigation'; // Use the useRouter hook for programmatic navigation
+import { useRouter } from 'next/navigation'; 
 import Image from 'next/image';
 
 export default function LocationCard({ location }) {
-  const router = useRouter(); // Initialize the router
-
+  const router = useRouter(); 
   const handleNavigate = () => {
-    // Navigate to the location detail page using router.push
     router.push(`/location/${location.id}`);
   };
 
   return (
     <div 
       onClick={handleNavigate} 
-      className="relative bg-white/90 backdrop-blur-md rounded-2xl overflow-hidden shadow-lg hover:shadow-2xl transition transform hover:scale-105 cursor-pointer flex flex-col"
+      className="relative bg-[var(--et)] backdrop-blur-md rounded-2xl overflow-hidden shadow-lg hover:shadow-2xl transition transform hover:scale-105 cursor-pointer flex flex-col"
     >
-      {/* Image */}
-      <div className="w-full h-50 relative">
+      <div className="w-full relative aspect-[16/9]">
         {location.image && (
           <Image
             src={`https://maps.googleapis.com/maps/api/place/photo?maxwidth=400&photo_reference=${encodeURIComponent(location.image)}&key=${process.env.NEXT_PUBLIC_GOOGLE_MAPS_API_KEY}`}
@@ -26,7 +23,6 @@ export default function LocationCard({ location }) {
             style={{ objectFit: "cover" }}
             onError={(e) => {
               console.error("Error loading image for", location.name, e);
-              // Optionally display a fallback image here
             }}
           />
         )}
@@ -37,9 +33,8 @@ export default function LocationCard({ location }) {
         )}
       </div>
 
-      {/* Info */}
       <div className="p-4 text-center">
-        <h2 className="text-lg font-semibold text-[#4B2E83]">{location.name}</h2>
+        <h2 className="text-lg font-medium text-[var(--background)]">{location.name}</h2>
       </div>
     </div>
   );
